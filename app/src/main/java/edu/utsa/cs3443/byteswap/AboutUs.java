0257faecd58cee1b36e3ac8fd18248fdce6b1568
Jibril-Pa <109011292+Jibril-Pa@users.noreply.github.com>
2024-11-30 02:16:38 -0600

    Created About the Team Page

---
+++ app/src/main/java/edu/utsa/cs3443/byteswap/AboutUs.java
@@ -0,0 +1,38 @@
+package edu.utsa.cs3443.byteswap;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AboutUs extends AppCompatActivity {
+    private Button backButton;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_us_page);
+        setBackButton();
+
+
+    }
+
+
+    private void setBackButton(){
+        backButton = findViewById(R.id.back_button);
+        if (backButton != null) {
+            backButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish(); // Closes the Settings activity and returns to MainActivity
+                }
+            });
+        } else {
+            Toast.makeText(this, "Back button not found", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+
+}
--- app/src/main/java/edu/utsa/cs3443/byteswap/Settings.java
+++ app/src/main/java/edu/utsa/cs3443/byteswap/Settings.java
@@ -1,5 +1,6 @@
 package edu.utsa.cs3443.byteswap;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,18 +9,41 @@ import android.widget.Toast;
 public class Settings extends AppCompatActivity {
 
     private Button backButton;
+    private Button aboutUsbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_controller);
+        setAboutUs();
+        setBackButton();
 
+    }
+
+
+    private void setBackButton(){
         backButton = findViewById(R.id.back_button);
         if (backButton != null) {
             backButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     finish(); // Closes the Settings activity and returns to MainActivity
+                }
+            });
+        } else {
+            Toast.makeText(this, "Back button not found", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private void setAboutUs(){
+        aboutUsbutton = findViewById(R.id.about_Us);
+        if(aboutUsbutton != null){
+            aboutUsbutton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent Intent = new Intent(Settings.this, AboutUs.class);
+                    startActivity(Intent);// Closes the Settings activity and returns to MainActivity
                 }
             });
         } else {
