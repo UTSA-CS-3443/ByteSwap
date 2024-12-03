5057fe5e76d47b96f3f0e77e944cc00e0c83191b
RRiveraa2001 <raina.rivera@my.utsa.edu>
2024-12-03 04:02:21 -0600

    Add files via upload

---
+++ DM/DM/app/src/main/java/edu/utsa/cs3443/dm/MainActivity.java
@@ -0,0 +1,36 @@
+package edu.utsa.cs3443.dm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupButton(R.id.button);
+    }
+
+
+
+    public void onClick(View view)
+    {
+        if (view.getId() == R.id.button)
+        {
+            Intent intent = new Intent(this, DMActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    public void setupButton(int buttonId)
+    {
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+}
