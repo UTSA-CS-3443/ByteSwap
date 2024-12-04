84fc60b02f4ee3c8cc613a6ac7522b578586a866
RRiveraa2001 <raina.rivera@my.utsa.edu>
2024-12-03 18:42:22 -0600

    adding file

--- app/src/main/java/edu/utsa/cs3443/myapplication/DMActivity.java
+++ app/src/main/java/edu/utsa/cs3443/myapplication/DMActivity.java
@@ -1,86 +1,52 @@
 package edu.utsa.cs3443.myapplication;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class DMActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+
+public class DMActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ListView list;
+    private edu.utsa.cs3443.dm.ListViewAdapter adapter;
+    private ArrayList<String[]> arrayList;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_messages);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_dmactivity);
+        setupButton(R.id.backButton);
+        list = findViewById(R.id.messagesList);
+
+        arrayList = new ArrayList<String[]>();
+
+
+        arrayList.add(new String[]{"Brooke", "Hello this is my last message"});
+        arrayList.add(new String[]{"Raina", "I am done"});
+        arrayList.add(new String[]{"Jib", "Hello this is my last message"});
+        arrayList.add(new String[]{"Reese", "Hello this is my last message"});
+        arrayList.add(new String[]{"Iqra", "Hello this is my last message"});
+
+        adapter = new edu.utsa.cs3443.dm.ListViewAdapter(this, arrayList);
+        list.setAdapter(adapter);
     }
 
-//        private static final String TAG = "DMActivity";
-//
-//        /**
-//         * Initialises the Twitter factory instance used to authenticate and create new Twitter object instances.
-//         */
-//        private static final DMActivity factory = new DMActivity(
-//                new ConfigurationBuilder()
-//                        .setDebugEnabled(true)
-//                        .setOAuthConsumerKey("<your app's API key>")
-//                        .setOAuthConsumerSecret("<your app's API secret>")
-//                        .setOAuthAccessToken("<a user's access token for your app>")
-//                        .setOAuthAccessTokenSecret("<a user's access token secret for your app>")
-//                        .setTweetModeExtended(true)
-//                        .build());
-//
-//        public <DirectMessageList> Object DMActivity (int count, String nextCursor) {
-//            if (count < 1 || count > 50) {
-//                Log.e(TAG, "Invalid input - count must be between 1 and 50.");
-//                return null;
-//            }
-//            DirectMessage directmessage = factory.getInstance();
-//            DirectMessageList messages;
-//            try {
-//                if (nextCursor == null) {
-//                    messages = directmessage.getDirectMessages(count);
-//                } else {
-//                    messages = directmessage.getDirectMessages(count, nextCursor);
-//                }
-//            } catch (TwitterException e) {
-//                Log.e(TAG, "Failed to retrieve direct messages from Twitter API", e);
-//                // You can implement specific error handling for some Twitter error codes here, since e.g. server overload you could
-//                // wait and try again instead of returning null
-//                return null;
-//            }
-//            return messages;
-//        }
-//
-//        public DirectMessageList getAllAvailableDirectMessages() {
-//            int maxCount = 50;
-//            DirectMessageList messages = getLastXDirectMessages(maxCount, null);
-//            if (messages == null) {
-//                return messages;
-//            }
-//            String nextCursor = messages.getNextCursor();
-//            while (nextCursor != null && !nextCursor.equals("")) {
-//                try {
-//                    Thread.sleep(60 * 1000);
-//                } catch (Exception e) {
-//                    Log.e(TAG, "Interrupted", e);
-//                    return null;
-//                }
-//                DirectMessageList nextList = getLastXDirectMessages(maxCount, nextCursor);
-//                if (nextList == null) {
-//                    return messages;
-//                }
-//                nextCursor = nextList.getNextCursor();
-//                messages.addAll(nextList);
-//            }
-//            return messages;
-//        }
-//
-//    }
+    public void onClick(View view)
+    {
+        if (view.getId() == R.id.backButton)
+        {
+            finish();
+        }
     }
+
+    public void setupButton(int buttonId)
+    {
+        Button button = findViewById(buttonId);
+        button.setOnClickListener(this);
+    }
+}
--- app/src/main/java/edu/utsa/cs3443/myapplication/MainActivity.java
+++ app/src/main/java/edu/utsa/cs3443/myapplication/MainActivity.java
@@ -1,30 +1,24 @@
 package edu.utsa.cs3443.myapplication;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import edu.utsa.cs3443.myapplication.DMActivity;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
         setupButton(R.id.button);
     }
+
+
 
     public void onClick(View view)
     {
@@ -35,10 +29,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-
     public void setupButton(int buttonId)
     {
-        Button button = findViewById(buttonId);
+        Button button = findViewById(R.id.button);
         button.setOnClickListener(this);
     }
+
 }
