7b281eb76059090db7a8bb534ac90de9f0e73123
iqra7861 <142940121+iqra7861@users.noreply.github.com>
2024-12-10 23:02:16 -0600

    Javadoc updates to Account.java

--- app/src/main/java/edu/utsa/cs3443/byteswap/Account.java
+++ app/src/main/java/edu/utsa/cs3443/byteswap/Account.java
@@ -12,15 +12,33 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+/**
+ * Represents the Account fragment, allowing the user to input a status and post it. 
+ */
 public class Account extends Fragment {
-
+    
+    /** EditText for entering the user's status. */
     private EditText statusInput;
+
+    /** Button for posting the user's status. */
     private Button postButton;
 
+     /**
+     * Default constructor for the Account fragment.
+     * Required to ensure the fragment can be instantiated properly.
+     */
     public Account() {
         // Required empty public constructor
     }
 
+    /**
+     * Called to create and return the view hierarchy associated with the fragment.
+     *
+     * @param inflater The LayoutInflater object that can be used to inflate views in the fragment.
+     * @param container The parent view that the fragment's UI should be attached to (if applicable).
+     * @param savedInstanceState If non-null, this fragment is being re-constructed from a previous state.
+     * @return The View for the fragment's UI, or null if not applicable.
+     */
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -42,8 +60,6 @@ public class Account extends Fragment {
                     // Show a toast with the status message
                     Toast.makeText(getContext(), "Posted: " + statusText, Toast.LENGTH_SHORT).show();
 
-                    // Clear the input field after posting
-                    statusInput.setText("");
                 } else {
                     // Inform the user if the status is empty
                     Toast.makeText(getContext(), "Please enter a status!", Toast.LENGTH_SHORT).show();
