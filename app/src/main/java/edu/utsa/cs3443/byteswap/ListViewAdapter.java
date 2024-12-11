9672ecd85d9bc52af2fd08652880fdfdb3913586
RRiveraa2001 <raina.rivera@my.utsa.edu>
2024-12-10 23:06:43 -0600

    Update ListViewAdapter.java

--- app/src/main/java/edu/utsa/cs3443/byteswap/ListViewAdapter.java
+++ app/src/main/java/edu/utsa/cs3443/byteswap/ListViewAdapter.java
@@ -9,29 +9,70 @@ import android.widget.TextView;
 
 import java.util.List;
 
+/**
+ * Custom adapter for displaying a list of contacts with a name and a message.
+ * This class extends {@link android.widget.BaseAdapter} to handle a custom ListView.
+ */
 public class ListViewAdapter extends BaseAdapter {
+    /**
+     * Context of the calling activity or fragment.
+     */
     private Context context;
+     /**
+     * List of string arrays, where each array contains contact name and message.
+     */
     private List<String[]> dataList;
+     /**
+     * Constructor to initialize the adapter with context and data.
+     *
+     * @param context The context of the calling activity or fragment.
+     * @param dataList The data to populate the ListView.
+     */
 
     public ListViewAdapter(Context context, List<String[]> dataList) {
         this.context = context;
         this.dataList = dataList;
     }
+    /**
+     * Returns the total number of items in the data list.
+     *
+     * @return The size of the data list.
+     */
 
     @Override
     public int getCount() {
         return dataList.size();
     }
+    /**
+     * Returns the data item at a specific position.
+     *
+     * @param position The index of the data item.
+     * @return The data item at the specified position.
+     */
 
     @Override
     public Object getItem(int position) {
         return dataList.get(position);
     }
+    /**
+     * Returns the row ID of the data item at a specific position.
+     *
+     * @param position The index of the data item.
+     * @return The row ID, which is the position itself.
+     */
 
     @Override
     public long getItemId(int position) {
         return position;
     }
+     /**
+     * Creates and returns the view for a specific data item.
+     *
+     * @param position The index of the data item.
+     * @param convertView The old view to reuse, if possible.
+     * @param parent The parent view that this view will attach to.
+     * @return The view representing the data item.
+     */
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
