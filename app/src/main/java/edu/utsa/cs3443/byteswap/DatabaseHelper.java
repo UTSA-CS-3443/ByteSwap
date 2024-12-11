7eb03ab279dd726b9ef983bcb96650791bab095e
RRiveraa2001 <raina.rivera@my.utsa.edu>
2024-12-10 23:16:51 -0600

    Update DatabaseHelper.java

--- app/src/main/java/edu/utsa/cs3443/byteswap/DatabaseHelper.java
+++ app/src/main/java/edu/utsa/cs3443/byteswap/DatabaseHelper.java
@@ -6,19 +6,47 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+/**
+ * A helper class to manage database creation, version management, and user-related operations.
+ */
 public class DatabaseHelper extends SQLiteOpenHelper {
 
+    /**
+     * Name of the database file.
+     */
     private static final String DATABASE_NAME = "users.db";
+    /**
+     * Database version. Increment this value when the schema changes.
+     */
     private static final int DATABASE_VERSION = 1;
 
+    /**
+     * Name of the table to store user information.
+     */
     private static final String TABLE_USERS = "users";
+    /**
+     * Column name for storing usernames.
+     */
     private static final String COLUMN_USERNAME = "username";
+    /**
+     * Column name for storing passwords.
+     */
     private static final String COLUMN_PASSWORD = "password";
 
+    /**
+     * Constructs a new instance of {@link DatabaseHelper}.
+     *
+     * @param context The context to use for locating paths to the database.
+     */
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+    /**
+     * Called when the database is created for the first time.
+     *
+     * @param db The database.
+     */
     @Override
     public void onCreate(SQLiteDatabase db) {
         String createTable = "CREATE TABLE " + TABLE_USERS + " (" +
@@ -27,12 +55,26 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL(createTable);
     }
 
+    /**
+     * Called when the database needs to be upgraded.
+     *
+     * @param db         The database.
+     * @param oldVersion The old database version.
+     * @param newVersion The new database version.
+     */
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_USERS);
         onCreate(db);
     }
 
+    /**
+     * Adds a new user to the database.
+     *
+     * @param username The username of the user.
+     * @param password The password of the user.
+     * @return {@code true} if the user was added successfully, {@code false} otherwise.
+     */
     public boolean addUser(String username, String password) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
@@ -42,6 +84,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         return result != -1;
     }
 
+     /**
+     * Validates a user's credentials by checking the database.
+     *
+     * @param username The username of the user.
+     * @param password The password of the user.
+     * @return {@code true} if the credentials are valid, {@code false} otherwise.
+     */
     public boolean validateUser(String username, String password) {
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor cursor = db.query(TABLE_USERS,
